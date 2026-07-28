6d08a72ce3c73786bc11de55370476a9a64024dc
Riaanshah09 <Riaan.Shah@sharkninja.com>
2026-07-28 12:53:23 -0700

    Add files via upload

--- start_sensors.bat
+++ start_sensors.bat
@@ -1,4 +1,9 @@
 @echo off
+echo REMINDER: Make sure all 3 sensor modules are turned on!
+echo.
+echo Plug in the USB cable now if you haven't already.
+echo.
+pause
 cd %USERPROFILE%\Desktop
 python pressure.py
 pause
